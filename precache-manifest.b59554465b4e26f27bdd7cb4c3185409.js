c50c8aa72ae67cc66937ff8b1c3267a5168f6e1b
J <socept@gmail.com>
2019-03-26 14:25:56 -0400

    Updates

---
+++ precache-manifest.b59554465b4e26f27bdd7cb4c3185409.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "75ea76a73ccb27733a5a",
+    "url": "/song-desc-react/static/js/runtime~main.08091314.js"
+  },
+  {
+    "revision": "368f0710a95aac67bee0",
+    "url": "/song-desc-react/static/js/main.315c5fb2.chunk.js"
+  },
+  {
+    "revision": "f0da5163b2565980f52a",
+    "url": "/song-desc-react/static/js/2.8d693f5d.chunk.js"
+  },
+  {
+    "revision": "588d233026a5cc24d347b08a1d141be0",
+    "url": "/song-desc-react/index.html"
+  }
+];
